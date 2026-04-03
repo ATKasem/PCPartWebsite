df56e3cef75668a72eec5a3f44da36785ac0c400
Aaron Thomas <aaronthomas@Aarons-HT-Macbook-Pro.local>
2026-04-03 17:02:56 -0500

    added UI

--- builds.js
+++ builds.js
@@ -6,30 +6,30 @@ const builds = [
     Storage: 100,
     GPU: 1,
     price: 100,
-    FPS: 60,    
-    games: {      
-  'Apex Legends': { 
-      settings: 'low', 
-      performance: { '1080p': 60, '1440p': 45, '4K': 30 } 
+    FPS: 60,
+    games: {
+      'Apex Legends': {
+        settings: 'low',
+        performance: { '1080p': 60, '1440p': 45, '4K': 30 }
+      },
+      'Battlefield 6': {
+        settings: 'low',
+        performance: { '1080p': 60, '1440p': 45, '4K': 30 }
+      },
+      'Call of Duty: Warzone': {
+        settings: 'low',
+        performance: { '1080p': 60, '1440p': 45, '4K': 30 }
+      }
+    },
   },
-  'Battlefield 6': { 
-      settings: 'low', 
-      performance: { '1080p': 60, '1440p': 45, '4K': 30 } 
-  },
-  'Call of Duty: Warzone': { 
-      settings: 'low', 
-      performance: { '1080p': 60, '1440p': 45, '4K': 30 } 
-  }
-}
-],
-
-   {
+  {
     name: 'build-2',
     CPU: 4,
     RAM: 8,
     Storage: 200,
     GPU: 2,
     price: 200,
+    FPS: 120,
     games: {
       'Apex Legends': {
         settings: 'Medium',
@@ -52,6 +52,7 @@ const builds = [
     Storage: 500,
     GPU: 4,
     price: 400,
+    FPS: 240,
     games: {
       'Apex Legends': {
         settings: 'High',
@@ -68,3 +69,5 @@ const builds = [
     }
   }
 ];
+
+module.exports = builds;
